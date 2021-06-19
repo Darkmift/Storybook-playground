113b7aa18f21e404a8388b18373d427b43f8251b
darkmift <darkmift@gmail.com>
2021-06-19 17:36:48 +0300

    fixed story for tasks

--- src/stories/Task.stories.js
+++ src/stories/Task.stories.js
@@ -5,28 +5,26 @@ import Task from '../components/Task';
 export default {
   title: 'Example/Task',
   component: Task,
-  argTypes: {
-    // onPinTask: {},
-    // onArchiveTask: {},
-    // task: {
-    //   id: '1',
-    //   title: 'Test Task',
-    //   state: 'TASK_INBOX',
-    //   updatedAt: new Date(2018, 0, 1, 9, 0),
-    // },
-  },
+  // argTypes: {
+
+  //   // onPinTask: action('pin-task'),
+  //   // onArchiveTask: action('archive-task'),
+  //   // onPinTask: {},
+  //   // onArchiveTask: {},
+
+  // },
 };
 
-// export const actionsData = {
-//   onPinTask: action('pin-task'),
-//   onArchiveTask: action('archive-task'),
-// };
+
 
 const Template = (args, { argTypes }) => ({
   components: { Task },
   props: Object.keys(argTypes),
-  // ...actionsData,
   template: '<Task v-bind="$props" v-on="$props"/>',
+  // methods: {
+  //   onPinTask: action('pin-task'),
+  //   onArchiveTask: action('archive-task'),
+  // }
 });
 
 export const Primary = Template.bind({});
@@ -37,16 +35,14 @@ Primary.args = {
     state: 'TASK_INBOX',
     updatedAt: new Date(2018, 0, 1, 9, 0),
   },
+  pinTask: action('pin-task'),
+  archiveTask: action('archive-task'),
 };
 
 export const Pinned = Template.bind({});
 Pinned.args = {
   task: {
-    // ...Primary.args.task,
-    id: '1',
-    title: 'Test Task',
-    state: 'TASK_INBOX',
-    updatedAt: new Date(2018, 0, 1, 9, 0),
+    ...Primary.args.task,
     state: 'TASK_PINNED',
   },
 };
@@ -54,11 +50,7 @@ Pinned.args = {
 export const Archived = Template.bind({});
 Archived.args = {
   task: {
-    // ...Primary.args.task,
-    id: '1',
-    title: 'Test Task',
-    state: 'TASK_INBOX',
-    updatedAt: new Date(2018, 0, 1, 9, 0),
+    ...Primary.args.task,
     state: 'TASK_ARCHIVED',
   },
 };
